348eea8f3bec5b919ad6f2c374adabd9c099e4a1
Michal Špaček <tupinek@gmail.com>
2015-01-08 20:00:28 +0100

    Prepare for 0.04 version.

--- Line.pm
+++ Line.pm
@@ -17,7 +17,7 @@ Readonly::Scalar our $LOG_REGEXP => qr{(\d{4}-\d{2}-\d{2})\s+(\d{2}:\d{2}:\d{2}\
 Readonly::Scalar our $SPACE => q{ };
 
 # Version.
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Parse FreeSWITCH log line.
 sub parse {
@@ -188,6 +188,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
--- Line/Data.pm
+++ Line/Data.pm
@@ -11,7 +11,7 @@ use Error::Pure qw(err);
 use Mo qw(builder is required);
 
 # Version.
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 has date => (
 	'is' => 'ro',
@@ -261,6 +261,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
